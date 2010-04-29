b7cca09a86088ef9b17e6c45ef66e703ab56f784
Grzegorz Śliwiński <fizyk@fizyk-laptop.(none)>
2010-04-29 21:56:23 +0200

    i18n string for "email change" confirmation message

--- modules/sfApply/templates/_sendValidateEmailText.php
+++ modules/sfApply/templates/_sendValidateEmailText.php
@@ -1,15 +1,15 @@
 <?php use_helper('I18N', 'Url') ?>
-<?php echo __('Hello %USERNAME%!
+<?php echo __("Hello %USERNAME%!
 
 We have received your request to change your email address on: %1%
 
-You\'re goint to change your email from: %OLDEMAIL% to %NEWEMAIL%
+You're going to change your email from: %OLDEMAIL% to %NEWEMAIL%
 
 To continue with your change, click on the link that follows:
 
 %2%
 
-Your email will then be changed pemanently.'
+Your email will then be changed pemanently."
 , array(
   "%1%" => $sf_request->getHost(),
   "%2%" => url_for("sfApply/confirm?validate=$validate", true),
