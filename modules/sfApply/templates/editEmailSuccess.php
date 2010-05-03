ca60e2a110ebb20ee15eb8c6908d0b5c0dcbe509
Grzegorz Śliwiński <fizyk@fizyk-laptop.(none)>
2010-05-03 13:40:40 +0200

    Updated editEmail template

--- modules/sfApply/templates/editEmailSuccess.php
+++ modules/sfApply/templates/editEmailSuccess.php
@@ -1,6 +1,15 @@
 <?php //TODO! fix this later ?>
-<form method="POST" action="<?php echo url_for("sfApply/editEmail") ?>" name="sf_apply_settings_form" id="sf_apply_settings_form">
+<?php use_stylesheets_for_form( $form ) ?>
+<?php use_helper("I18N") ?>
+<div class="sf_apply sf_apply_settings">
+<h2><?php echo __("Edit Email", array(), 'sfForkedApply') ?></h2>
+<form method="POST" action="<?php echo url_for("sfApply/editEmail") ?>" name="sf_apply_email_edit_form" id="sf_apply_email_edit_form">
 <ul>
 <?php echo $form ?>
 <li>
-<input type="submit" value="<?php echo __("Save", array(), 'sfForkedApply') ?>" />
+<input type="submit" value="<?php echo __("Save", array(), 'sfForkedApply') ?>" /> <?php echo(__("or", array(), 'sfForkedApply')) ?>
+ <?php echo link_to(__('Cancel', array(), 'sfForkedApply'), sfConfig::get('app_sfApplyPlugin_after', '@settings')) ?>
+</li>
+</ul>
+</form>
+</div>
