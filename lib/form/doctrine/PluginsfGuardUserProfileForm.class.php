3fb422d3fe9b932c4f9aa4caf4b7b6a02e2be8da
Grzegorz Śliwiński <fizyk@fizyk.net.pl>
2010-07-06 22:05:34 +0200

    changed model definitions and files

--- lib/form/doctrine/PluginsfGuardUserProfileForm.class.php
+++ lib/form/doctrine/PluginsfGuardUserProfileForm.class.php
@@ -1,27 +1,23 @@
 <?php
 
 /**
- * PluginsfGuardUserProfileBasis form.
+ * PluginsfGuardUserProfile form.
  *
  * @package    ##PROJECT_NAME##
  * @subpackage form
  * @author     ##AUTHOR_NAME##
  * @version    SVN: $Id: sfDoctrineFormPluginTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
  */
-abstract class PluginsfGuardUserProfileBasisForm extends BasesfGuardUserProfileBasisForm
+abstract class PluginsfGuardUserProfileForm extends BasesfGuardUserProfileForm
 {
     public function setup()
     {
         parent::setup();
         sfProjectConfiguration::getActive()->loadHelpers(array('I18N'));
-        
     }
-
-
-
+    
     public function getStylesheets()
     {
         return array( '/sfForkedDoctrineApplyPlugin/css/forked' => 'all' );
     }
-    
 }
