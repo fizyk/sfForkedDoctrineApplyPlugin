80485522cb52cba6fd9751bf194a7c3479843d19
Grzegorz Śliwiński <fizyk@fizyk.net.pl>
2010-07-30 18:15:51 +0200

    cleaning

--- lib/form/sfApplyApplyForm.class.php
+++ lib/form/sfApplyApplyForm.class.php
@@ -6,8 +6,8 @@
  */
 class sfApplyApplyForm extends sfGuardUserProfileForm
 {
-    private $validate = null;
-
+    private $userId;
+    
     public function configure()
     {
         parent::configure();
