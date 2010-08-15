a79e400a3cc7515e60f8fc3b69e41e3561df538f
Grzegorz Śliwiński <fizyk@fizyk.net.pl>
2010-08-15 21:44:48 +0200

    moved sfApply components to BasesfApplyComponents class

--- modules/sfApply/actions/components.class.php
+++ modules/sfApply/actions/components.class.php
@@ -8,18 +8,8 @@
  * @version    SVN: $Id: actions.class.php 2692 2006-11-15 21:03:55Z fabien $
  */
 
+require_once dirname(__FILE__).'/../lib/BasesfApplyComponents.class.php';
 
-class sfApplyComponents extends sfComponents
+class sfApplyComponents extends BasesfApplyComponents
 {
-    public function executeLogin()
-    {
-        $this->loggedIn = $this->getUser()->isAuthenticated();
-        if (!$this->loggedIn)
-        {
-            $class = sfConfig::get('app_sf_guard_plugin_signin_form',
-                    'sfGuardFormSignin');
-            $this->form = new $class();
-            $this->form->getWidgetSchema()->setFormFormatterName('list');
-        }
-    }
 }
